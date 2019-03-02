089f533ca0a9766b749e0a2a9f4908c205b8243c
DrewBanyai <DrewBanyai@gmail.com>
2019-03-01 23:35:15 -0800

    - Added the ProgressBar UI Element type with Standard template, which will be needed in the next rendition of New Providence - Updated Color.h with a copy constructor and some more default colors to pick from - Added a render-with-cutoff to TemplatedBox in GUIObjectNode.h

--- Client/NewProvidence/Engine/Color.h
+++ Client/NewProvidence/Engine/Color.h
@@ -4,10 +4,18 @@ struct Color
 {
 	Color(float r = 1.0f, float g = 1.0f, float b = 1.0f, float a = 1.0f)
 	{
-		colorValues[0] = r;
-		colorValues[1] = g;
-		colorValues[2] = b;
-		colorValues[3] = a;
+		R = r;
+		G = g;
+		B = b;
+		A = a;
+	}
+
+	Color(const Color& other)
+	{
+		R = other.R;
+		G = other.G;
+		B = other.B;
+		A = other.A;
 	}
 
 	union {
@@ -18,3 +26,8 @@ struct Color
 
 static Color COLOR_WHITE(1.0f, 1.0f, 1.0f, 1.0f);
 static Color COLOR_WHITE_FADED(1.0f, 1.0f, 1.0f, 0.25f);
+static Color COLOR_RED(1.0f, 0.0f, 0.0f, 1.0f);
+static Color COLOR_GREEN(0.0f, 1.0f, 0.0f, 1.0f);
+static Color COLOR_BLUE(0.0f, 0.0f, 1.0f, 1.0f);
+static Color COLOR_BLACK(0.0f, 0.0f, 0.0f, 1.0f);
+static Color COLOR_GRAY(0.4f, 0.4f, 0.4f, 1.0f);
